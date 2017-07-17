d8ce2492064f975cf213282add6a798b0e7fddcb
Olayinka_Peter <olayinkapeter99@gmail.com>
2017-07-17 04:24:13 +0100

    Initial commit

---
+++ app/src/main/java/com/olayinkapeter/popularmovies1/MoviesApi/API.java
@@ -0,0 +1,17 @@
+package com.olayinkapeter.popularmovies1.MoviesApi;
+
+/**
+ * Created by Fransunisoft on 19/03/16.
+ */
+public class API {
+
+    static public String API_URL = "http://api.themoviedb.org/3/";
+    static public String APPEND = "discover/movie";
+    static public String SORT_TYPE_POPULAR = "movie/popular";
+    static public String SORT_TYPE_MOST_VIEWED = "movie/popular";
+    static public String API_KEY = "b88b4d42b7d73770429715903a8559eb";
+    static public String IMAGE_URL = "http://image.tmdb.org/t/p/";
+    static public String IMAGE_SIZE_185 = "w185";
+    static public String IMAGE_NOT_FOUND = "http://i.imgur.com/N9FgF7M.png";
+
+}
